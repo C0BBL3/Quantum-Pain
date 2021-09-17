1dd00310dcb3c515b598088c4c974ec12b9a11b7
Colby Roberts <colbiverse@gmail.com>
2021-09-17 12:42:59 -0700

    Dad's commit

--- addition.py
+++ addition.py
@@ -1,5 +1,8 @@
 from bit import Bit
 from qubit import Qubit
+import numpy as np
+import math
+
 
 def add(num_1, num_2): # `num_1` and `num_2` are an array of `Qubits` or `Bits` but not both in the same list
     try:
@@ -8,33 +11,58 @@ def add(num_1, num_2): # `num_1` and `num_2` are an array of `Qubits` or `Bits` 
                 raise Exception('Mixed Qubit and Bit string')
             else:
                 if isinstance(num_1[0], Qubit) and isinstance(num_2[0], Qubit):
-                    return quantum_pain(num_1, num_2)
-            if len(num_1) == 1 and len(num_2) == 1:
-                if num_1[0].value + num_2[0].value > 1:
-                    return [Bit(1), Bit(0)]
+                    return quantum(num_1, num_2)
                 else:
-                    return [Bit(num_1[0].value + num_2[0].value)]
-            longer_list_len = len(max(len(num_1), len(num_2)))
-            result = []
-            carry = 0
-            for index in range(longer_list_len - 1, -1, -1):
-                temp = carry
-                temp += 1 if num_1[index] == 1 else 0
-                temp += 1 if num_2[index] == 1 else 0
-                result = [(Bit(1) if temp % 2 == 1 else Bit(0))] + result
-            
-                carry = 0 if temp < 2 else 1
-            
-            if carry != 0:
-                result = [Bit(1)] + result  
-
-            return result
+                    return binary(num_1, num_2)
         elif not isinstance(list, num_1) and not isinstance(list, num_2):
             return num_1 + num_2
         raise ValueError('Please give the function two numbers of the same type. Either both lists of Qubits/Bits or both floats/ints.')
     except ValueError as va:
         print(va)
         exit()
+
+def quantum(num_1, num_2): # recursive addition... yeah... recursive... addition... kill me now
+    if len(num_1) == 1 and len(num_2) == 1:
+        return two_qubits(num_1[0], num_2[0])
+    # A Density Matrix a day keeps Dr. Chapman away... yay
+    
+
+def two_qubits(is_entangled, qubit_1, qubit_2): # I think this is how it be
+    if is_entangled:
+        probability_vector = np.append(qubit_1.probability_vector, qubit_2.probability_vector, axis=0)
+    else:
+        probability_vector = np.array([value_1 * value_2 for value_2 in qubit_2.probability_vector for value_1 in qubit_1.probability_vector])
+    qubits = [qubit_1, qubit_2]
+    gap = 0
+    for qubit in qubits:
+        for i in range(0,2):
+            gubit.probability_vector = np.array([[probability_vector[i][0]], [probability_vector[i + 1 + gap][0]]])
+        gap = 1
+    return qubits
+
+
+def binary(num_1, num_2):
+    if len(num_1) == 1 and len(num_2) == 1:
+        if num_1[0].value + num_2[0].value > 1:
+            return [Bit(1), Bit(0)]
+        else:
+            return [Bit(num_1[0].value + num_2[0].value)]
+    longer_list_len = len(max(len(num_1), len(num_2)))
+    result = []
+    carry = 0
+    for index in range(longer_list_len - 1, -1, -1):
+        temp = carry
+        temp += 1 if num_1[index] == 1 else 0
+        temp += 1 if num_2[index] == 1 else 0
+        result = [(Bit(1) if temp % 2 == 1 else Bit(0))] + result
+    
+        carry = 0 if temp < 2 else 1
+    
+    if carry != 0:
+        result = [Bit(1)] + result  
+
+    return result + [0 for _ in range(0, len(longer_list_len) - len(result))]
+
 
 def check_if_list_is_mixed(num_list):
     qubit_count = [isinstance(num, Qubit) for num in num_list].count(True)
--- qubit.py
+++ qubit.py
@@ -1,15 +1,25 @@
 import math
+from random import choices
+import Bit
+import numpy as np
 
 class Qubit:
     def __init__(self):
         self.value = None
         self.entangled_pains = []
+        # Initial Values (subject to change)
+        self.probability_vector = np.array([(1 / (math.sqrt(2))), (1 / (math.sqrt(2)))])
+        self.update_probabilities()
 
-    def observe(self, probability_values): # Larry
-        self.probability_for_0 = sum([math.abs(value) ^ 2 for value in probability_values])
+    #def observe(self, probability_values): # Larry
+        
+
+    def update_probabilities(self):
+        self.probability_for_0 = sum([math.abs(value) ^ 2 for value in self.probability_vector]) 
         self.probability_for_1 = 1.0 - self.probability_for_0
 
     def __getitem__(self, value):
-        return fhieowbifew[]
+        choice = choices([Bit(0), Bit(1)], [self.probability_for_0, self.probability_for_1])
+        return choice
     
     
