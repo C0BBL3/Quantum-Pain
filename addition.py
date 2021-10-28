b4cfef9772c7c7cc391d0f52de52059c4dfb03a5
Colby Roberts <colbiverse@gmail.com>
2021-10-27 19:53:02 -0700

    addition fully works

--- addition.py
+++ addition.py
@@ -3,6 +3,14 @@ import numpy as np
 import math
 from random import choices
 
+def generate_quantum_system(bit_string):
+    quantum_system = []
+    for bit in bit_string:
+        if bit == '0' or bit == 0:
+            quantum_system.append(Qubit(probability_vector = [1,0]))
+        if bit == '1' or bit == 1:
+            quantum_system.append(Qubit(probability_vector = [0,1]))
+    return quantum_system
 
 def add(num_1, num_2): # `num_1` and `num_2` are an array of `Qubits` or `Bits` but not both in the same list
     try:
@@ -21,36 +29,41 @@ def add(num_1, num_2): # `num_1` and `num_2` are an array of `Qubits` or `Bits` 
         print(va)
         exit()
 
-def quantum(num_1, num_2):
-    if len(num_1) == 1 and len(num_2) == 1:
-        return ''.join(list(two_qubit_quantum_system(num_1[0], num_2[0])))
-    else:
-        if len(num_1) == len(num_2):
-            result = []
-            carry = [Qubit(probability_vector = [1,0]) for _ in range(len(num_1) + 1)]
-            for i, (q_1, q_2) in enumerate(list(zip(num_1, num_2))[::-1]):
-                sum_ = two_qubit_quantum_system(q_1, q_2, carry[i], carry[i+1])
-                result.append(sum_[0])
-                if i == 0:
-                    first_carry = sum_[1]
-                if sum_[1] == '1':
-                    carry[i+1] = Qubit(probability_vector = [0,1])
-                    result.append(sum_[1])
-            result = [first_carry] + result
-            return ''.join(result[::-1])
+def quantum(qs_1, qs_2):
+    if len(qs_1) == 0 and len(qs_2) == 0:
+        return '0'
+    elif len(qs_1) == 1 and len(qs_2) == 1:
+        sum_ = two_qubits(qs_1[0], qs_2[0])
+        if sum_[1] == '0':
+            return sum_[0]
         else:
-            return 'fuck you im not doing this now'
+            return ''.join(sum_[::-1])
+    else:
+        if len(qs_1) > len(qs_2):
+            qs_2 = generate_quantum_system(''.join(['0' for _ in range(len(qs_1) - len(qs_2))])) + qs_2
+        elif len(qs_2) > len(qs_1):
+            qs_1 = generate_quantum_system(''.join(['0' for _ in range(len(qs_2) - len(qs_1))])) + qs_1
+        result = []
+        carry = [Qubit(probability_vector = [1,0]) for _ in range(len(qs_1) + 1)]
+        for i, (q_1, q_2) in enumerate(list(zip(qs_1, qs_2))[::-1]):
+            sum_ = two_qubits(q_1, q_2, carry[i], carry[i+1])
+            result.append(sum_[0])
+            if sum_[1] == '1':
+                carry[i+1] = Qubit(probability_vector = [0,1])
+        if sum_[1] == '1':
+            result.append(sum_[1])
+        return ''.join(result[::-1])
 
-def two_qubit_quantum_system(num_1, num_2, c_in = Qubit(probability_vector = [1,0]), zero = Qubit(probability_vector = [1,0])): # recursive addition... yeah... recursive... addition... kill me now
+def two_qubits(q_1, q_2, c_in = Qubit(probability_vector = [1,0]), zero = Qubit(probability_vector = [1,0])): # recursive addition... yeah... recursive... addition... kill me now
     # This is the full adder algorithm by Prof Feyman
-    probability_matrix_a_b_c_in_zero = generate_probability_matrix([num_1] + [num_2] + [c_in] + [Qubit(probability_vector = [1, 0])])
+    probability_matrix_a_b_c_in_zero = generate_probability_matrix([q_1] + [q_2] + [c_in] + [zero])
     new_probability_matrix = ccnot(probability_matrix_a_b_c_in_zero, swaps=[0,1,3]) # Each of the following operations returns a matrix with a probability for each possible outcome. 
     new_probability_matrix = cnot(new_probability_matrix, swaps=[0,1])
     new_probability_matrix = ccnot(new_probability_matrix, swaps=[1,2,3])
     new_probability_matrix = cnot(new_probability_matrix, swaps=[1,2])
     new_probability_matrix = cnot(new_probability_matrix, swaps=[0,1]) # optional
     observation = observe(new_probability_matrix) # Returns a list of bits
-    return observation[3], observation[2]
+    return observation[2], observation[3] # sum, carry
 
 def generate_probability_matrix(qubits): #Tensor Multiplication
     k = 2 ** len(qubits)
@@ -94,6 +107,10 @@ def binary(num_1, num_2):
         elif num_1[0] + num_2[0] <= 1:
             return str(num_1[0] + num_2[0])
     longer_list_len = max(len(num_1), len(num_2))
+    if len(num_1) > len(num_2):
+        num_2 = [0 for _ in range(longer_list_len - len(num_2))] + num_2
+    if len(num_2) > len(num_1):
+        num_1 = [0 for _ in range(longer_list_len - len(num_1))] + num_1
     bit_string = []
     carry = 0
     for index in range(longer_list_len - 1, -1, -1):
@@ -104,7 +121,7 @@ def binary(num_1, num_2):
         carry = 0 if temp < 2 else 1
     if carry != 0:
         bit_string = [1] + bit_string  
-    return ''.join([str(bit) for bit in bit_string] + ['0' for _ in range(0, longer_list_len - len(bit_string))])
+    return ''.join([str(bit) for bit in bit_string] + ['0' for _ in range(longer_list_len - len(bit_string))])
 
 
 def check_if_list_is_mixed(num_list):
--- test.py
+++ test.py
@@ -1,29 +1,45 @@
 from addition import add
+from addition import generate_quantum_system
 from qubit import Qubit
-import math
 
-#answers are in binary, so 10 = [1, 0] = 2
+#answers are in binary, so 10  =  [1, 0]  =  10
 
-print('0+0=0')
-print(add([0], [0])) # 1+0=1
-print(add([Qubit(probability_vector = [1,0])], [Qubit(probability_vector = [1,0])])) # 1+0=1
+print('\n0 + 0 = 0') # 0 + 0 = 0
+print('Conventional:', add([0], [0])) # 0 + 0 = 0
+print('Quantum:', add(generate_quantum_system('0'), generate_quantum_system('0'))) # 0 + 0 = 0
 
-print('1+0=1')
-print(add([0], [1])) # 1+0=1
-print(add([Qubit(probability_vector = [1,0])], [Qubit(probability_vector = [0,1])])) # 1+0=1
+print('\n0 + 1 = 1') # 0 + 1 = 0
+print('Conventional:', add([0], [1])) # 0 + 1 = 1
+print('Quantum:', add(generate_quantum_system('0'), generate_quantum_system('1'))) # 0 + 1 = 1
 
-print('0+1=1')
-print(add([1], [0])) # 0+1=1
-print(add([Qubit(probability_vector = [0,1])], [Qubit(probability_vector = [1,0])])) # 0+1=1
+print('\n1 + 0 = 1') # 0 + 1 = 1
+print('Conventional:', add([1], [0])) # 0 + 1 = 1
+print('Quantum:', add(generate_quantum_system('1'), generate_quantum_system('0'))) # 0 + 1 = 1
 
-print('1+1=10')
-print(add([1], [1])) # 1+1=10
-print(add([Qubit(probability_vector = [0,1])], [Qubit(probability_vector = [0,1])])) # 1+1=10
+print('\n1 + 1 = 2 = 10') # 1 + 1 = 2
+print('Conventional:', add([1], [1])) # 1 + 1 = 10
+print('Quantum:', add(generate_quantum_system('1'), generate_quantum_system('1'))) # 1 + 1 = 10
 
-#print('2+1=11')
-#print(add([1,0], [1,0])) # 2+1=11
-#print(add([Qubit(probability_vector = [0,1]), Qubit(probability_vector = [1,0])], [Qubit(probability_vector = [0,1])])) # 2+1=11
+print('\n2 + 1 = 3 = 11') # 2 + 1 = 3
+print('Conventional:', add([1,0], [1])) # 10 + 1 = 11
+print('Quantum:', add(generate_quantum_system('10'), generate_quantum_system('1'))) # 10 + 1 = 11
 
-print('2+2=100')
-print(add([1,0], [1,0])) # 2+2=100
-print(add([Qubit(probability_vector = [0,1]), Qubit(probability_vector = [1,0])], [Qubit(probability_vector = [0,1]), Qubit(probability_vector = [1,0])])) # 2+2=100
+print('\n2 + 2 = 4 = 100') # 2 + 2 = 4
+print('Conventional:', add([1,0], [1,0])) # 10 + 10 = 100
+print('Quantum:', add(generate_quantum_system('10'), generate_quantum_system('10'))) # 10 + 10 = 100
+
+print('\n6 + 4 = 10 = 1010') # 6 + 4 = 10
+print('Conventional:', add([1,1,0], [1,0,0])) # 110 + 100 = 1010
+print('Quantum:', add(generate_quantum_system('110'), generate_quantum_system('100'))) # 110 + 100 = 1010
+
+print('\n6 + 12 = 18 = 10010') 
+print('Conventional:', add([1,1,0], [1,1,0,0])) # 110 + 1100 = 10010
+print('Quantum:', add(generate_quantum_system('110'), generate_quantum_system('1100'))) # 110 + 1100 = 10010
+
+print('\n35 + 23 = 58 = 111010')
+print('Conventional:', add([1,0,0,0,1,1], [1,0,1,1,1])) # 100011 + 10111 = 111010
+print('Quantum:', add(generate_quantum_system('100011'), generate_quantum_system('10111'))) # 100011 + 10111 = 111010
+
+print('\n1234 + 4321 = 5555 = 1010110110011')
+print('Conventional:', add([1,0,0,1,1,0,1,0,0,1,0], [1,0,0,0,0,1,1,1,0,0,0,0,1])) # 100011 + 10111 = 1010110110011
+print('Quantum:', add(generate_quantum_system('10011010010'), generate_quantum_system('1000011100001'))) # 100011 + 10111 = 1010110110011
